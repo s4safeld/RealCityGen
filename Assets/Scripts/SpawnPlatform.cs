28501d8ed4676f926e430933310ad25cd9f158a4
s4safeld <sascha.feld@gmx.net>
2020-06-02 11:42:46 +0200

    Added camera distance checker to the VFF functionality

--- Assets/Scripts/SpawnPlatform.cs
+++ Assets/Scripts/SpawnPlatform.cs
@@ -7,20 +7,47 @@ public class SpawnPlatform : MonoBehaviour
 {
     public GameObject spawnBlock;
     public GameObject spawnedBlock;
-
-    private new Vector3 pos;
+    public Camera mainCam;
+    
+    private bool isSpawned = false;
+    private bool isVisible = false;
+    //private int viewDistance;
 
     private void Start()
     {
-        pos = gameObject.transform.position;
+        mainCam = Camera.main;
+    }
+    public void Update()
+    {
+        if (Vector3.Distance(transform.position, mainCam.transform.position) >= 100)
+        {
+            Destroy(spawnedBlock);
+            isSpawned = false;
+        }
+        else
+        {
+            if (!isSpawned && isVisible)
+            {
+                spawnedBlock = Instantiate(spawnBlock, transform.position + new Vector3(0, 0.5f, 0), Quaternion.identity);
+                isSpawned = true;
+            }
+                
+        }
     }
 
     void OnBecameVisible()
     {
-        spawnedBlock = Instantiate(spawnBlock, pos + new Vector3(0, 0.5f, 0), Quaternion.identity);
+        isVisible = true;
+        if (Vector3.Distance(transform.position, mainCam.transform.position) <= 100 && isSpawned == false)
+        {
+            spawnedBlock = Instantiate(spawnBlock, transform.position + new Vector3(0, 0.5f, 0), Quaternion.identity);
+            isSpawned = true;
+        } 
     }
     void OnBecameInvisible()
     {
+        isVisible = false;
         Destroy(spawnedBlock);
+        isSpawned = false;
     }
 }
