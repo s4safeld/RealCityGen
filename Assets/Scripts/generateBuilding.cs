db8a09d01e5477ee820fddfe4705e67ae79eff73
Sascha Feld <sascha.feld@gmx.net>
2020-08-17 13:49:20 +0200

    Finished Random Mesh generation Algorithm. next up: Textures and UVs

--- Assets/Scripts/generateBuilding.cs
+++ Assets/Scripts/generateBuilding.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class generateBuilding : MonoBehaviour
 {
@@ -13,6 +14,11 @@ public class generateBuilding : MonoBehaviour
     public Material material;
     public int edges;
     public Vector3 shapeMesh;
+    public int seed;
+    public int minCount;
+    public int maxCount;
+    public float minRange;
+    public float maxRange;
 
     // Start is called before the first frame update
     void Start()
@@ -38,22 +44,39 @@ public class generateBuilding : MonoBehaviour
          */
 
 
+        //Temporary
+        if (seed == 0) {
+            seed = Random.Range(-100000, 100000);
+        }
+        Random.InitState(seed);
+        Debug.Log("seed: "+seed);
+        //--------
 
-        Mesh mesh = compute2DMesh();
-        Mesh mesh3 = extrudeMesh(mesh, shapeMesh);
+        Mesh randMesh = randomMesh(minCount,maxCount,minRange,maxRange);
+        //Mesh mesh = compute2DMesh();
+        //Mesh mesh3 = extrudeMesh(mesh, shapeMesh);
         
 
-        GameObject gameObject = new GameObject("Mesh", typeof(MeshFilter), typeof(MeshRenderer));
-        gameObject.GetComponent<MeshFilter>().mesh = mesh;
+        //GameObject gameObject = new GameObject("Mesh", typeof(MeshFilter), typeof(MeshRenderer));
+        //gameObject.GetComponent<MeshFilter>().mesh = mesh;
 
-        GameObject gameObject3 = new GameObject("Mesh3", typeof(MeshFilter), typeof(MeshRenderer));
-        gameObject3.GetComponent<MeshFilter>().mesh = mesh3;
+        //GameObject gameObject3 = new GameObject("Mesh3", typeof(MeshFilter), typeof(MeshRenderer));
+        //gameObject3.GetComponent<MeshFilter>().mesh = mesh3;
+
+        GameObject gameObject4 = new GameObject("randomMesh", typeof(MeshFilter), typeof(MeshRenderer));
+        gameObject4.GetComponent<MeshFilter>().mesh = randMesh;
+
+
+        GameObject gameObject5 = new GameObject("randomMesh2", typeof(MeshFilter), typeof(MeshRenderer));
+        gameObject5.GetComponent<MeshFilter>().mesh = randMesh;
+
+
 
         Vector3 pos;
 
         float x = 0;
 
-        GameObject temp;
+        /*GameObject temp;
         temp = GameObject.CreatePrimitive(PrimitiveType.Sphere);
         temp.name = "Pi";
         temp.transform.position = new Vector3(Mathf.Sin(3.141f), 0, -1);
@@ -65,9 +88,58 @@ public class generateBuilding : MonoBehaviour
             temp = GameObject.CreatePrimitive(PrimitiveType.Sphere);
             temp.name = "Sphere" + i;
             temp.transform.position = new Vector3(Mathf.Sin(i),0,Mathf.Cos(i));
-        }
+        }*/
     }
 
+    private void Update()
+    {
+        Debug.DrawLine(new Vector3(0, -10, 0), new Vector3(0, 10, 0), Color.red);
+    }
+    Mesh randomMesh(int minCount, int maxCount, float minRange, float maxRange) {
+
+        maxCount = Mathf.RoundToInt(Random.Range(minCount, maxCount));
+
+        Vector2[] vertices2D = new Vector2[maxCount];
+
+        float stepSize = 8 / (float)maxCount;
+
+        float steps = 0;
+        for (int i = 0; i < maxCount; i++) {
+            float x = Mathf.Sin(Random.Range(steps, steps + stepSize));
+            float z = Mathf.Cos(Random.Range(steps, steps + stepSize));
+            vertices2D[i] = new Vector2(x, z);
+            vertices2D[i] *= Random.Range(minRange, maxRange);
+            steps += stepSize;
+            Debug.Log("Steps: " + steps);
+        }
+
+        Triangulator tr = new Triangulator(vertices2D);
+        int[] indices = tr.Triangulate();
+
+
+        Vector3[] vertices = new Vector3[maxCount];
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            vertices[i] = new Vector3(vertices2D[i].x, 0, vertices2D[i].y);
+            Debug.Log("vertices[" + i + "]: " + vertices[i]);
+        }
+
+        Vector2[] uvs = new Vector2[vertices.Length];
+        for (int i = 0; i < uvs.Length; i++) {
+            uvs[i] = new Vector2(vertices[i].x, vertices[i].z);
+        }
+
+        Mesh mesh = new Mesh();
+        mesh.vertices = vertices;
+        mesh.triangles = indices;
+        mesh.uv = uvs;
+
+        mesh.RecalculateNormals();
+        mesh.RecalculateBounds();
+
+
+        return mesh;
+    }
 
 
     Mesh compute2DMesh() {
@@ -212,4 +284,122 @@ public class generateBuilding : MonoBehaviour
         return mesh;
     }
 
+}
+
+public class Triangulator
+{
+    private List<Vector2> m_points = new List<Vector2>();
+
+    public Triangulator(Vector2[] points)
+    {
+        m_points = new List<Vector2>(points);
+    }
+
+    public int[] Triangulate()
+    {
+        List<int> indices = new List<int>();
+
+        int n = m_points.Count;
+        if (n < 3)
+            return indices.ToArray();
+
+        int[] V = new int[n];
+        if (Area() > 0)
+        {
+            for (int v = 0; v < n; v++)
+                V[v] = v;
+        }
+        else
+        {
+            for (int v = 0; v < n; v++)
+                V[v] = (n - 1) - v;
+        }
+
+        int nv = n;
+        int count = 2 * nv;
+        for (int v = nv - 1; nv > 2;)
+        {
+            if ((count--) <= 0)
+                return indices.ToArray();
+
+            int u = v;
+            if (nv <= u)
+                u = 0;
+            v = u + 1;
+            if (nv <= v)
+                v = 0;
+            int w = v + 1;
+            if (nv <= w)
+                w = 0;
+
+            if (Snip(u, v, w, nv, V))
+            {
+                int a, b, c, s, t;
+                a = V[u];
+                b = V[v];
+                c = V[w];
+                indices.Add(a);
+                indices.Add(b);
+                indices.Add(c);
+                for (s = v, t = v + 1; t < nv; s++, t++)
+                    V[s] = V[t];
+                nv--;
+                count = 2 * nv;
+            }
+        }
+
+        indices.Reverse();
+        return indices.ToArray();
+    }
+
+    private float Area()
+    {
+        int n = m_points.Count;
+        float A = 0.0f;
+        for (int p = n - 1, q = 0; q < n; p = q++)
+        {
+            Vector2 pval = m_points[p];
+            Vector2 qval = m_points[q];
+            A += pval.x * qval.y - qval.x * pval.y;
+        }
+        return (A * 0.5f);
+    }
+
+    private bool Snip(int u, int v, int w, int n, int[] V)
+    {
+        int p;
+        Vector2 A = m_points[V[u]];
+        Vector2 B = m_points[V[v]];
+        Vector2 C = m_points[V[w]];
+        if (Mathf.Epsilon > (((B.x - A.x) * (C.y - A.y)) - ((B.y - A.y) * (C.x - A.x))))
+            return false;
+        for (p = 0; p < n; p++)
+        {
+            if ((p == u) || (p == v) || (p == w))
+                continue;
+            Vector2 P = m_points[V[p]];
+            if (InsideTriangle(A, B, C, P))
+                return false;
+        }
+        return true;
+    }
+
+    private bool InsideTriangle(Vector2 A, Vector2 B, Vector2 C, Vector2 P)
+    {
+        float ax, ay, bx, by, cx, cy, apx, apy, bpx, bpy, cpx, cpy;
+        float cCROSSap, bCROSScp, aCROSSbp;
+
+        ax = C.x - B.x; ay = C.y - B.y;
+        bx = A.x - C.x; by = A.y - C.y;
+        cx = B.x - A.x; cy = B.y - A.y;
+        apx = P.x - A.x; apy = P.y - A.y;
+        bpx = P.x - B.x; bpy = P.y - B.y;
+        cpx = P.x - C.x; cpy = P.y - C.y;
+
+        aCROSSbp = ax * bpy - ay * bpx;
+        cCROSSap = cx * apy - cy * apx;
+        bCROSScp = bx * cpy - by * cpx;
+
+        return ((aCROSSbp >= 0.0f) && (bCROSScp >= 0.0f) && (cCROSSap >= 0.0f));
+    }
 }
